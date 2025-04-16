cc47afbd35a0dd90aa44f22d846801af34a4c1ca
Admin <felixwebbo8@gmail.com>
2025-04-16 11:54:58 +0300

    made some modification

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,17 +1,15 @@
-// app/api/ping/route.ts
 import { NextResponse } from "next/server";
-import prisma from "@/lib/client";
+import prisma from "@/lib/client"; // or wherever your prisma client is
 
+//to test the db connection
 export async function GET() {
   try {
-    const result = await prisma.user.findMany({
-      take: 1,
-    });
-    return NextResponse.json({ message: "DB Connected!", user: result });
-  } catch (error) {
-    console.error("DB Connection Error:", error);
+    await prisma.$connect();
+    return NextResponse.json({ message: "Connected to DB!" });
+  } catch (err) {
+    console.error("DB Connection Error:", err);
     return NextResponse.json(
-      { error: "Failed to connect to DB" },
+      { error: "Failed to connect to DB \n check you DB" },
       { status: 500 }
     );
   }
