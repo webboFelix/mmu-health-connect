dab9c01c49d2d991aff027258edae5423b51632a
Admin <felixwebbo8@gmail.com>
2025-04-16 11:41:11 +0300

    made some modification

--- src/app/api/db/test-db.ts
+++ src/app/api/db/test-db.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/client"; // or wherever your prisma client is
 
+//to test the db connection
 export async function GET() {
   try {
     await prisma.$connect();
